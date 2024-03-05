e5e919d31745246f713a7dba3eb2b79e572e248a
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-05 22:33:33 +0000

    Added the task 0 file and the config

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const success = true;
+
+			if (success) {
+				resolve("API response data");
+			} else {
+				reject(new Error("API request failed"));
+			}
+		}, 1000);
+	});
+}
