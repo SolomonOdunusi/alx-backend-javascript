41e4fa4d40a5c75a5a79ad7bf146be165cbf1f3b
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-06 01:14:35 +0000

    Fixed task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.all([
-		signUpUser(firstName, lastName),
-		uploadPhoto(fileName),
-	]).then((results) => {
-		return results.map((result, index) => ({
-			status: index === 0 ? "user created" : "photo uploaded",
-			value: result,
-		}));
-	});
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result, index) => ({
+    status: index === 0 ? 'user created' : 'photo uploaded',
+    value: result,
+  })));
 }
