b1fc0750ec73e75e278cc1f3f3b59d356e4b1b27
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-06 00:02:05 +0000

    Worked on a function for promise all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+	const photoPromise = uploadPhoto();
+	const userPromise = createUser();
+
+	Promise.all([photoPromise, userPromise])
+	.then(([photoData, userData]) => {
+		console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+	})
+	.catch((error) => {
+		console.log("Signup system offline");
+	});
+}
