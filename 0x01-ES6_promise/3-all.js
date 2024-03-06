20e4fcb853c38cf338015b52e85e7318364c5b5f
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-06 20:56:07 +0000

    Removed the initial definitions

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
-
-  try {
-    Promise.all([photoPromise, userPromise])
-      .then(([photoData, userData]) => {
-        console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-      })
-      .catch(() => {
-        console.log('Signup system offline');
-      });
-  } catch (error) {
-    console.error('Error handling profile signup:', error);
-  }
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
