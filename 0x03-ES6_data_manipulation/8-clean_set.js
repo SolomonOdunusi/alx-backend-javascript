8178be6c79b25c6dae8d68c005354e8ca7a5a484
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-12 05:22:14 +0000

    Fixed lint errors and some import/export parsing issue

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,8 +3,8 @@ export default function cleanSet(set, startString) {
 
   if (
     typeof set !== 'object'
-		|| typeof startString !== 'string'
-		|| startString.length === 0
+      || typeof startString !== 'string'
+      || startString.length === 0
   ) {
     return '';
   }
